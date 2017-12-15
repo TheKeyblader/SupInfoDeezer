da3a9fa88abf0592aa242323130e6f42656c7e59
CyrilleGuichard <34371782+CyrilleGuichard@users.noreply.github.com>
2017-12-15 15:40:20 +0100

    Design vFin

--- SupinfoDeezer/MainWindow.xaml.cs
+++ SupinfoDeezer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Api;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SupinfoDeezer
 {
@@ -20,19 +23,86 @@ namespace SupinfoDeezer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Track> Track { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
             mainFrame.Navigate(new Uri("TrackContainer.xaml", UriKind.RelativeOrAbsolute));
+            DataContext = this;
+            Track = new ObservableCollection<Track>();
+            Track.Add(new Track
+            {
+                Title_Short = "",
+                Title_Version = "",
+                Artist = new Artist
+                {
+                    Name = ""
+                },
+                Album = new Album
+                {
+                    Cover = ""
+                },
+            });
         }
 
         private async void BarreRecherche_TextChanged(object sender, TextChangedEventArgs e)
         {
             var textbox = (TextBox)sender;
-            if(textbox.Text != null && textbox.Text != "")
+
+            if(textbox.Text != null && textbox.Text != "" && mainFrame != null)
             {
-                await ((TrackContainer)mainFrame.Content).GetAlbums(textbox.Text);
+                if(mainFrame.Content is AlbumContainer)
+                {
+                    await ((AlbumContainer)mainFrame.Content).GetAlbums(textbox.Text);
+                }
+                else if(mainFrame.Content is TrackContainer)
+                {
+                    await ((TrackContainer)mainFrame.Content).GetTracks(textbox.Text);
+                }
             }
+        }
+
+        private async void SwitchToTracks(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Content = null;
+            mainFrame.Navigate(new Uri("TrackContainer.xaml", UriKind.RelativeOrAbsolute));
+            mainFrame.NavigationService.Refresh();
+            mainFrame.Loaded += new RoutedEventHandler(LoadTracks);
+        }
+
+        private async void LoadTracks(object sender, RoutedEventArgs e)
+        {
+            if (BarreRecherche.Text != null && BarreRecherche.Text != "")
+            {
+                await ((TrackContainer)mainFrame.Content).GetTracks(BarreRecherche.Text);
+            }
+            else
+            {
+                await ((TrackContainer)mainFrame.Content).GetTracks("Daft Punk");
+            }
+        }
+
+        private void SwitchToAlbums(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Content = null;
+            mainFrame.Source = new Uri("AlbumContainer.xaml", UriKind.RelativeOrAbsolute);
+            //mainFrame.Navigate(new Uri("AlbumContainer.xaml", UriKind.RelativeOrAbsolute));
+            mainFrame.NavigationService.Refresh();
+            mainFrame.Loaded += new RoutedEventHandler(LoadAlbums);
+        }
+
+        private async void LoadAlbums(object sender, RoutedEventArgs e)
+        {
+            if (BarreRecherche.Text != null && BarreRecherche.Text != "")
+            {
+                await ((AlbumContainer)mainFrame.Content).GetAlbums(BarreRecherche.Text);
+            }
+            else
+            {
+                await ((AlbumContainer)mainFrame.Content).GetAlbums("Daft Punk");
+            }
+            mainFrame.Loaded -= LoadAlbums;
         }
     }
 }
