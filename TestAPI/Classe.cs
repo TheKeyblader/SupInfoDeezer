1071623644c38e93e544a2fb86d6afc221579ca4
zamiquie <aymericbaquet@gmail.com>
2017-12-15 16:29:40 +0100

    azer(tyu

--- TestAPI/Classe.cs
+++ TestAPI/Classe.cs
@@ -5,20 +5,66 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Api;
+using API;
 using System.Net.Http.Headers;
+using System.IO;
 
-namespace TestAPI
+
+namespace API
 {
-    public class ApiDezzer
+    public class Authentification
     {
-      public int Id { get; set; }//Id musique de Deezer.com
-      public string Title { get; set; } // titreMusique
-      public string Singer { get; set; } //NomChanteur
-      public string Album { get; set; } // Album chanson
-      public string PublishYear { get; set; } // Annee parution
+        public string App_ID = "263102";
+        public string App_SECREt = "45c24250c1e2be4fa836f9a92aabb4fa";
+        public string MyURL = "www.deezer.com";
+        public string NumCOde { get; set; }
+
+
+
+        public void Authorisation()
+        {
+            System.Diagnostics.Process.Start("https://connect.deezer.com/oauth/auth.php?app_id=263102&redirect_uri=http://www.deezer.com&perms=basic_access,manage_library,delete_library,listening_history,access_token"); 
+        }
+
+
+
+
+        public void Token()
+        {
+            Console.WriteLine("N°CodeDeezer"); 
+            NumCOde = Console.ReadLine();
+            //requete d'envois 
+            WebRequest Access_Token = WebRequest.Create("https://connect.deezer.com/oauth/access_token.php?app_id="+App_ID+"&secret="+App_SECREt+"&code="+NumCOde);
+            Access_Token.Method = "T";
+            HttpWebResponse ReceptionToken = (HttpWebResponse)Access_Token.GetResponse();
+            Stream DataToken = ReceptionToken.GetResponseStream();
+            Console.WriteLine(ReceptionToken);
+            StreamReader reader = new StreamReader(DataToken); //Lecture de la Requete
+            string StringReponse = reader.ReadToEnd();// Convertion en String                                               
+            Console.WriteLine(StringReponse);
+            Console.ReadLine();
+        }
 
     }
 
-    
+    public class ModifPlaylist
+    {
+        public string NameListe { get; set; }
+        
+
+
+        public void CreatePlaylist (string IDUser, string PlayslistName)
+        {
+            WebRequest Create_Liste = WebRequest.Create("https://api.deezer.com/user/" + IDUser + "/playlists?title="+ PlayslistName);
+            Create_Liste.Method = "POST";
+            HttpWebResponse ReceptionPost = (HttpWebResponse)Create_Liste.GetResponse();
+            Stream DataCreation = ReceptionPost.GetResponseStream();
+            Console.WriteLine(ReceptionPost);
+            StreamReader reader = new StreamReader(DataCreation);
+            string StringReponse = reader.ReadToEnd();
+            Console.WriteLine(StringReponse); 
+            
+            
+        }
+    }
 }
--- TestAPI/ProgramTest2.cs
+++ TestAPI/ProgramTest2.cs
@@ -5,31 +5,35 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Api;
+using API;
 using System.Net.Http.Headers;
 using System.IO;
 using Newtonsoft.Json;
 
-namespace TestAPI
+
+namespace API
 {
 
     class Program
     {
         static void Main(string[] args)
         {
-           //Microsoft.IdentityModel.Protocols.OpenIdConnect.OpenIdConnectConfiguration 
+            Authentification Login = new Authentification();
 
-
+            Login.Authorisation();
+            Login.Token();
             
-            //System.Diagnostics.Process.Start("https://connect.deezer.com/oauth/auth.php?app_id=263102&redirect_uri=http://www.deezer.com&perms=basic_access,manage_library,delete_library,access_token"); // Ouverture de la page WEB, dans le navigateur par default. 
-                                                                                                                                                                                                          // Requete Post 
+
+           ModifPlaylist Modif = new ModifPlaylist();
+            Modif.CreatePlaylist("9070265", "grosTest");
+           
+
+            /*// Requete POST (modification)
             WebRequest Modif = WebRequest.Create("https://api.deezer.com/user/9070265/playlists/?title=test");
-            Modif.Method = "POST"; 
-
-
-
-
-
+            Modif.Method = "POST";
+            HttpWebResponse ResponseModif = (HttpWebResponse)Modif.GetResponse();
+            //Stream respose = Modif.GetRequestStream();
+            Console.WriteLine(ResponseModif.StatusDescription);
 
             //Requete GET (obtention d'information) 
             Search IdCherche = new Search();
@@ -38,7 +42,7 @@ namespace TestAPI
             HttpWebResponse reponse = (HttpWebResponse)request.GetResponse(); // variable de reception de la requete 
             request.Method = "POST"; // type de demande (get = obtenir, post = modifier, Delete)
             Stream dataStream = reponse.GetResponseStream(); // envoie de la requete
-            Console.WriteLine(reponse.StatusDescription);
+            //Console.WriteLine(reponse.StatusDescription);// retourne statut de la requete 
             StreamReader reader = new StreamReader(dataStream); //Lecture de la Requete
             string StringReponse = reader.ReadToEnd();// Convertion en String                                                  // Console.WriteLine(StringReponse);
             Console.WriteLine(StringReponse);
@@ -47,7 +51,7 @@ namespace TestAPI
             //Search requestedObject = JsonConvert.DeserializeObject<Search>(StringReponse); // convertit string en objet
             //Console.WriteLine(requestedObject.Id + " " + requestedObject.Title);// objet creer par la classe
             
-            Console.ReadLine();
+            Console.ReadLine();*/
 
         }
     }
@@ -67,54 +71,7 @@ namespace TestAPI
         //public Artiste[];
         //pubmic Album[];
 
-        public string SearchTrackByID(int ID)
-        {
-            return "https://api.deezer.com/track/" + ID.ToString();
-        }
-        public string SearchAlbumByID(int Album)
-        {
-            return "https://api.deezer.com/album/" + Album.ToString();
 
-        }
-        public string SearchPlaylistByID(long Playlist)
-        {
-            return "https://api.deezer.com/playlist/" + Playlist.ToString();
-        }
-        public string SearchArtisttByID(long Artist)
-        {
-            return "https://api.deezer.com/artist/" + Artist.ToString();
-        }
-
-
-
-        public string SearchTrack(string TrackName)
-        {
-            return "https://api.deezer.com/search/track?q=" + TrackName;
-
-        }
-        public string SearchArtist(string ArtistName)
-        {
-            return "https://api.deezer.com/search/artist?q=" + ArtistName;
-        }
-        public string SearchAlbum(string AlbumName)
-        {
-            return "https://api.deezer.com/search/album?q=" + AlbumName;
-        }
-        public string SearchTrack_Artist(string TrackName, string ArtistName)
-        {
-            return "https://api.deezer.com/search?q=track:" + TrackName + " artist:" + ArtistName;
-
-        }
-        public string SearchAlbum_Artist(string AlbumName, string ArtistName)
-        {
-            return "https://api.deezer.com/search?q=track:" + AlbumName + " artist:" + ArtistName;
-
-        }
-        public string SearchTrack_Album(string TrackName, string AlbumName)
-        {
-            return "https://api.deezer.com/search?q=track:" + TrackName + " artist:" + AlbumName;
-
-        }
         public string SearchUser(string User)
         {
             return "https://api.deezer.com/search?q=user:" + User;
@@ -123,7 +80,7 @@ namespace TestAPI
         {
             return "https://api.deezer.com/user/" + IDUser.ToString();
         }
-       
+
         public string SearchPlaylistByUser(int IDUser, string NameUser)
         {
             return "https://api.deezer.com/playlist/" + IDUser.ToString() + "/" + NameUser;
@@ -132,8 +89,7 @@ namespace TestAPI
 
 
     }
+
+
+
 }
-    
-
-
-
